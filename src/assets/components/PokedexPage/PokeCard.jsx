a99796c53d2e1178385f22acfb3ad8c054ebbf67
Christian Romero <43833120+ChrisJRM92@users.noreply.github.com>
2024-05-26 19:45:49 -0500

    Add features
    Responsive design, stats and movement

--- src/assets/components/PokedexPage/PokeCard.jsx
+++ src/assets/components/PokedexPage/PokeCard.jsx
@@ -18,6 +18,39 @@ const PokeCard = ({poke}) => {
     navigate(`/pokemon/${pokemon.name}`)
   }
 
+  const Labels_ES = {
+    hp: 'hp',
+    attack: 'Ataque',
+    defense: 'Defensa',
+    'special-attack': 'Ataque especial',
+    'special-defense': 'Defenza especial',
+    'speed': 'Velocidad'
+  }
+
+  const Types_Es ={
+    normal: 'Normal',
+    fighting: 'Lucha',
+    flying: 'Volador',
+    poison: 'Veneno',
+    ground: 'Tierra',
+    rock: 'Roca',
+    bug: 'Bicho',
+    ghost: 'Fantasma',
+    steel: 'Acero',
+    fire: 'Fuego',
+    water: 'Agua',
+    grass: 'Planta',
+    electric: 'Eléctrico',
+    psychic: 'Psíquico',
+    ice: 'Hielo',
+    dragon: 'Dragón',
+    dark: 'Siniestro',
+    fairy: 'Hada',
+    stellar: 'Estelar',
+    unknown: 'Desconocido',
+    shadow: 'Sombra'
+  }
+
   console.log(pokemon)
 
   return (
@@ -31,7 +64,7 @@ const PokeCard = ({poke}) => {
         <ul className="poke_types">
           {
           pokemon?.types.map( typeInfo => (
-            <li className="poke_types_item" key={typeInfo.type.url}> {typeInfo?.type.name}</li>
+            <li className="poke_types_item" key={typeInfo.type.url}> {Types_Es[typeInfo?.type.name] || typeInfo?.type.name}</li>
           ))
           }
         </ul>
@@ -41,7 +74,7 @@ const PokeCard = ({poke}) => {
           {
             pokemon?.stats.map( statInfo => (
               <li className="poke_stats_item" key={statInfo.stat.url}>
-                <span className="poke_stats_label">{statInfo.stat.name}</span>
+                <span className="poke_stats_label">{Labels_ES[statInfo.stat.name] || statInfo.stat.name}</span>
                 <span className={`poke_stats_value text_${pokemon?.types[0].type.name}`}>{statInfo.base_stat}</span>
               </li>
             ))
--- src/assets/components/PokedexPage/SelectType.jsx
+++ src/assets/components/PokedexPage/SelectType.jsx
@@ -12,19 +12,43 @@ const SelectType = ({settypeselected}) => {
     gettypes(url)
   }, [])
   
-  // console.log(types)
 
   const handleChange = (e) =>{
     settypeselected(e.target.value)
   }
 
+  const types_ES = {
+    normal: 'Normal',
+    fighting: 'Lucha',
+    flying: 'Volador',
+    poison: 'Veneno',
+    ground: 'Tierra',
+    rock: 'Roca',
+    bug: 'Bicho',
+    ghost: 'Fantasma',
+    steel: 'Acero',
+    fire: 'Fuego',
+    water: 'Agua',
+    grass: 'Planta',
+    electric: 'Eléctrico',
+    psychic: 'Psíquico',
+    ice: 'Hielo',
+    dragon: 'Dragón',
+    dark: 'Siniestro',
+    fairy: 'Hada',
+    stellar: 'Estelar',
+    unknown: 'Desconocido',
+    shadow: 'Sombra'
+  }
+  // console.log(types?.results[0].name)
+
   return (
     <select className="select__type" onChange={handleChange}>
-      <option className="type__options" value="allPokemons">All pokemons</option>
+      <option className="type__options" value="allPokemons">Mostrar todos</option>
       {
         types?.results.map(typeInfo =>(
           <option key={typeInfo.url} value={typeInfo.url}>
-            {typeInfo.name}
+            {types_ES[typeInfo.name] || typeInfo.name}
           </option>
         ))
       }
